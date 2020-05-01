ec45fe8c5b199afce3b142039f9c998a4e5a2948
timager <timger98@gmail.com>
2020-05-02 00:16:30 +0300

    key for FontAwesome

--- src/components/BaseElements/Cat/CatGender/CatGender.js
+++ src/components/BaseElements/Cat/CatGender/CatGender.js
@@ -5,18 +5,18 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 class   CatGender extends Component {
     render() {
         let gender;
-        let icon = ""
+        let icon;
         switch (this.props.gender) {
             case 'male':
-                icon = (this.props.icons) ? <FontAwesomeIcon icon={faMars}/> : "";
+                icon = (this.props.icons) ? <FontAwesomeIcon key={true} icon={faMars}/> : "";
                 gender = " кот"
                 break;
             case 'female':
-                icon = (this.props.icons) ? <FontAwesomeIcon icon={faVenus}/> : "";
+                icon = (this.props.icons) ? <FontAwesomeIcon key={true} icon={faVenus}/> : "";
                 gender = ' кошка';
                 break;
             default:
-                icon = (this.props.icons) ? <FontAwesomeIcon icon={faVenusMars}/> : "";
+                icon = (this.props.icons) ? <FontAwesomeIcon key={true} icon={faVenusMars}/> : "";
                 gender = 'Неизвестен';
         }
 
